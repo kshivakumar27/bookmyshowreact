7af67220dbb1c2ea71f372109d1308e47974e3d1
Your Name <you@example.com>
2022-08-10 23:08:04 +0530

    rel 1.5

--- src/api.js
+++ src/api.js
@@ -1,54 +1,54 @@
 import axios from "axios";
 
 export function Postlogin(data){
-    return axios.post(`http://bookmyshownodej.herokuapp.com/login`,data)
+    return axios.post(`https://bookmyshownodej.herokuapp.com/login`,data)
 }
 
 export function Postregister(data){
-    return axios.post(`http://bookmyshownodej.herokuapp.com/register`,data)
+    return axios.post(`https://bookmyshownodej.herokuapp.com/register`,data)
 }
 
 export function Postalogin(data){
-    return axios.post(`http://bookmyshownodej.herokuapp.com/alogin`,data)
+    return axios.post(`https://bookmyshownodej.herokuapp.com/alogin`,data)
 }
 
 export function Postaregister(data){
-    return axios.post(`http://bookmyshownodej.herokuapp.com/aregister`,data)
+    return axios.post(`https://bookmyshownodej.herokuapp.com/aregister`,data)
 }
 
 export function PostMovie(data){
-    return axios.post(`http://bookmyshownodej.herokuapp.com/movies`,data)
+    return axios.post(`https://bookmyshownodej.herokuapp.com/movies`,data)
 }
 
 export function PostTicket(data){
-    return axios.post(`http://bookmyshownodej.herokuapp.com/ticket`,data)
+    return axios.post(`https://bookmyshownodej.herokuapp.com/ticket`,data)
 }
 
 export function UpdateSeats(id,data){
-    return axios.put(`http://bookmyshownodej.herokuapp.com/movies/${id}`,data)
+    return axios.put(`https://bookmyshownodej.herokuapp.com/movies/${id}`,data)
 }
 
 export function GetMovie(){
-    return axios.get(`http://bookmyshownodej.herokuapp.com/movies`)
+    return axios.get(`https://bookmyshownodej.herokuapp.com/movies`)
 }
 
 export function GetMovieById(id){
-    return axios.get(`http://bookmyshownodej.herokuapp.com/movies/${id}`)
+    return axios.get(`https://bookmyshownodej.herokuapp.com/movies/${id}`)
 }
 
 export function GetUserByEmail(email){
-    return axios.get(`http://bookmyshownodej.herokuapp.com/users/${email}`)
+    return axios.get(`https://bookmyshownodej.herokuapp.com/users/${email}`)
 }
 
 export function GetTicketByName(username){
-    return axios.get(`http://bookmyshownodej.herokuapp.com/ticket/${username}`)
+    return axios.get(`https://bookmyshownodej.herokuapp.com/ticket/${username}`)
 }
 
 export function GetTickets(){
-    return axios.get(`http://bookmyshownodej.herokuapp.com/tickets`)
+    return axios.get(`https://bookmyshownodej.herokuapp.com/tickets`)
 }
 
 export function DeleteMovieById(id){
-    return axios.delete(`http://bookmyshownodej.herokuapp.com/movies/${id}`)
+    return axios.delete(`https://bookmyshownodej.herokuapp.com/movies/${id}`)
 }
 
